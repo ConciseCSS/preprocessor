5d77d2fd75586d39c88fc8b2a90b36c31e41430a
James Kolce <contact@jameskolce.com>
2017-11-11 20:55:09 -0700

    Fix package.json and recompile source files

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const lh_1 = require("./lib/lh");
+const custom_media_1 = require("./lib/custom-media");
+const media_minmax_1 = require("./lib/media-minmax");
+exports.default = file => {
+    const ccss = fs.readFileSync(file, 'utf8');
+    return postcss([
+        media_minmax_1.default,
+        custom_media_1.default,
+        lh_1.default,
+        autoprefixer
+    ]).process(ccss).css;
+};
